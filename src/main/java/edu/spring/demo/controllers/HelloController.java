56304ca28a4d35dd48cc4739b81492eb56983393
Mafeuh <mafmaf14.official@gmail.com>
2023-01-20 12:59:38 +0100

    Fin TD0

--- src/main/java/edu/spring/demo/controllers/HelloController.java
+++ src/main/java/edu/spring/demo/controllers/HelloController.java
@@ -2,27 +2,43 @@ package edu.spring.demo.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.view.RedirectView;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
+@SessionAttributes("user")
 public class HelloController {
-	
-	@GetMapping("/")
+	@GetMapping("/hello")
 	@ResponseBody
 	public String HelloAction() {
 		return "Hello World!";
 	}
-	
-	
-	@GetMapping("/msg/{content}")
+	@GetMapping(path={"/msg/{content}", "/msg"})
 	@ResponseBody
 	public String MsgAction(@PathVariable("content") String message) {
 		return "Message: " + message;
 	}
-	
-	@GetMapping("/view")
+	@RequestMapping(path="/",method={RequestMethod.POST, RequestMethod.GET})
 	public String ViewAction() {
-		return "viewName";
+		return "index";
+	}
+	@PostMapping("/login")
+	public RedirectView loginAction(HttpSession session, @ModelAttribute("username") String login) {
+		session.setAttribute("user", login);
+		return new RedirectView("/");
+	}
+	
+	@GetMapping("/logout")
+	public RedirectView logoutAction(HttpSession session) {
+		session.invalidate();
+		return new RedirectView("/");
 	}
 }
