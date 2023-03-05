e811b6685138d90698eef0fbfce864588fda70d4
Mafeuh <mafmaf14.official@gmail.com>
2023-03-05 19:34:13 +0100

    Vrai commit final, celui là c'est sûr c'est le dernier

--- src/main/kotlin/edu/spring/dogs/controllers/MainController.kt
+++ src/main/kotlin/edu/spring/dogs/controllers/MainController.kt
@@ -75,14 +75,13 @@ class MainController {
 		}
 		return RedirectView("/")
 	}
-	@GetMapping("master/{id}/delete")
+	@GetMapping("/master/{id}/delete")
 	fun deleteMaster(@PathVariable id:Int): RedirectView {
 		val master = masterRepos.findById(id).orElse(null)
 		if (master != null) {
 			for(dog in dogRepos.findAll()){
 				if(dog.master?.id == id) dog.master = null
 			}
-			//master.preRemove()
 			masterRepos.delete(master)
 		}
 		return RedirectView("/")
@@ -94,20 +93,20 @@ class MainController {
 			@RequestParam("choixMaitre") idMaitre: Int
 	):RedirectView{
 		val dog=dogRepos.findById(id).orElse(null)
-		val master=masterRepos.findById(idMaitre).orElse(null)
-		masterRepos.save(master)
-		if(master!=null){
 			when (action){
 				"remove" -> {
 					dogRepos.delete(dog)
 				}
 				"adopt" -> {
-					dog.master = master;
-					master.addDog(dog)
-					dogRepos.save(dog)
+					val master=masterRepos.findById(idMaitre).orElse(null)
+					if(master != null){
+						masterRepos.save(master)
+						dog.master = master;
+						master.addDog(dog)
+						dogRepos.save(dog)
+					}
 				}
 			}
-		}
 		return RedirectView("/")
 	}
 }
